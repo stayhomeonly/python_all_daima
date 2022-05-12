2406bb55a53ed61d8788e791f368f13ab4b42c1b
FENGXIN <1126731010@qq.com>
2022-05-12 20:23:01 +0800

    新增邮件发送的代码

--- Pythonselenium/class2_sendmail/maildemo.py
+++ Pythonselenium/class2_sendmail/maildemo.py
@@ -5,11 +5,55 @@
 #@Content:
 
 """
-# 需要使用到SMTPLIB库，来进行邮箱的连接
+import smtplib # 需要使用到SMTPLIB库，来进行邮箱的连接
+from email.mime.text import MIMEText # 处理内容的库，email.mime
+
 
 # 邮箱属性的配置
 mailserver = 'smtp.qq.com'  # 邮箱服务端URL
 userName_SendMail = "1126731010@qq.com"  # 发件人
 unserName_AuthCode = 'xpzljjypzobijbai'  # 邮件授权码
-receive_mail = ["1126731010@qq.com",
-                'm15161039042@163.com']  # 邮箱收件人如果后面还需要添加其他人，加个逗号，写入邮箱账号
+receive_mail = ["1126731010@qq.com"]  # 邮箱收件人如果后面还需要添加其他人，加个逗号，写入邮箱账号
+
+# 发送一封简单的邮件
+# content = "这是一封纯粹的文本信息内容"
+#
+# email=MIMEText(content,'plain','utf-8') # 纯文本形式的邮件内容定义
+#
+# email['Subject']='邮件主题'  # 定义邮件主题
+#
+# email['From']=userName_SendMail # 发件人
+# email['To']="1126731010@qq.com" # 收件人
+# # email['To']=','.join(receive_mail) # 如果多个收件人建议这种
+
+# 发送一封html的内容的邮件
+content="""
+<p>这是一封HTML文本的邮件</p>
+<p><a href="http://www.baidu.com">点击这里就送个锤子</a></p>
+"""
+email=MIMEText(content,'HTML','utf-8') # 纯文本形式的邮件内容定义
+
+email['Subject']='邮件主题_HTML'  # 定义邮件主题
+
+email['From']=userName_SendMail # 发件人
+email['To']="1126731010@qq.com" # 收件人
+# email['To']=','.join(receive_mail) # 如果多个收件人建议这种
+
+
+# 发送邮件
+
+smtp=smtplib.SMTP_SSL(mailserver,port=465)
+
+smtp.login(userName_SendMail,unserName_AuthCode)
+
+smtp.sendmail(userName_SendMail,','.join(receive_mail),email.as_string())
+
+
+smtp.quit()
+
+print("邮件发送成功")
+
+
+
+
+
